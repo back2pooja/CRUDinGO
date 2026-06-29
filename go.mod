c257f43b7a2331fe14238e8156d3827126d652d5
Pooja Jaiswar <pooja.jaiswar.987@gmail.com>
2026-06-29 11:31:54 +0530

    added initializer.go

--- go.mod
+++ go.mod
@@ -5,11 +5,13 @@ go 1.26
 require (
 	github.com/google/uuid v1.6.0
 	github.com/rs/cors v1.11.1
+	gorm.io/driver/sqlite v1.6.0
 	gorm.io/gorm v1.31.2
 )
 
 require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
 	golang.org/x/text v0.38.0 // indirect
 )
---
+++ initializer.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() error {
+	var err error
+	DB, err = gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+	DB.Logger = logger.Default.LogMode(logger.Info)
+
+	log.Println("Running Migrations...")
+	err = DB.AutoMigrate(&Note{})
+	if err != nil {
+		return err
+	}
+	log.Println("Connected Successfully to the database...")
+	return nil
+
+}
